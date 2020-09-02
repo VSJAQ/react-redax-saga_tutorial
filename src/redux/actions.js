04b882bbe3f82234959c848e43e7c95f031f0a2d
Erik Devyashin <vsjaq25@gmail.com>
2020-09-02 16:20:36 +0500

    developed alert for async post

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -20,9 +20,16 @@ export function hideLoader() {
 }
 
 export function showAlert(text) {
-    return {
-        type: SHOW_ALERT,
-        payload: text,
+    return dispatch => {
+        dispatch({
+            type: SHOW_ALERT,
+            payload: text,
+        })
+
+        setTimeout(
+            () => {
+                dispatch(hideAlert())
+            }, 3000)
     }
 }
 
@@ -32,12 +39,17 @@ export function hideAlert() {
     }
 }
 
-export function fetchPost(post) {
+export function fetchPost() {
     return async dispatch => {
-        dispatch(showLoader())
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-        const json = await response.json()
-        dispatch({ type: FETCH_POST, payload: json })
-        dispatch(hideLoader())
+        try {
+            dispatch(showLoader())
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+            const json = await response.json()
+            dispatch({type: FETCH_POST, payload: json})
+            dispatch(hideLoader())
+        } catch (e) {
+            dispatch(showAlert('Что-то пошло не так'))
+            dispatch(hideAlert())
+        }
     }
 }
